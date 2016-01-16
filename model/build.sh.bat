f348cecb4bd8f72750e0e03c1edc11f8d7c31302
Marko Elezovic <marko@mentat-labs.com>
2016-01-16 12:30:14 +0100

    Added test model + offline compiler

---
+++ model/build.sh.bat
@@ -0,0 +1,1 @@
+java -jar dsl-clc-1.5.1-SNAPSHOT.jar compiler=dsl-compiler-1.4.5841.42425.exe dsl=dsl settings=manual-json revenj.java=../lib/model.jar download
---
+++ model/dsl/test.dsl
@@ -0,0 +1,9 @@
+module test
+{
+  big root Transaction {
+    Timestamp   at;
+    Decimal(2)? in;
+    Decimal(2)? out;
+    String      description;
+  }
+}
